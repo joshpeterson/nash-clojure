de613ae48759445f83a988fe5d770abf57b00538
Joshua Peterson <petersonjm1@gmail.com>
2013-03-12 19:46:41 -0400

    Use pmap instead of my pmap implementation.

--- src/nash_clojure/core.clj
+++ src/nash_clojure/core.clj
@@ -39,23 +39,6 @@
 ; Nash game categorization
 ;
 
-(defn pmap-mine
-  "Like map, except f is applied in parallel. Semi-lazy in that the
-  parallel computation stays ahead of the consumption, but doesn't
-  realize the entire result unless required. Only useful for
-  computationally intensive functions where the time of f dominates
-  the coordination overhead."
-  {:added "1.0"
-   :static true}
-  ([n f coll]
-   (let [rets (map #(future (f %)) coll)
-         step (fn step [[x & xs :as vs] fs]
-                (lazy-seq
-                 (if-let [s (seq fs)]
-                   (cons (deref x) (step xs (rest s)))
-                   (map deref vs))))]
-     (step rets (drop n rets)))))
-
 (defn categorize-nash-solutions [player-one-strategy player-two-strategy]
   (count (take 2 (filter #{1} (vector-bit-and player-one-strategy player-two-strategy)))))
 
@@ -88,11 +71,10 @@
 (defn pcategorize-nash-games [number-of-rows number-of-columns number-of-partitions]
   (let [number-of-games (number-of-nash-games number-of-rows number-of-columns)]
     (into (sorted-map) (reduce #(merge-with + %1 %2)
-                               (pmap-mine number-of-partitions
-                                          #(categorize-given-nash-games number-of-rows number-of-columns %)
-                                          (partition-nash-games number-of-partitions number-of-rows
-                                                                number-of-columns
-                                                                (range number-of-games)))))))
+                               (pmap #(categorize-given-nash-games number-of-rows number-of-columns %)
+                                     (partition-nash-games number-of-partitions number-of-rows
+                                                           number-of-columns
+                                                           (range number-of-games)))))))
 ;
 ; Main function
 ;
