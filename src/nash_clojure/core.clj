e82706bae8195266b6154b5cf26afc4a159a9940
Joshua Peterson <petersonjm1@gmail.com>
2013-05-09 06:39:36 -0400

    Update the code to work with Clojure CLR - Add the source code for clojure.tools.cli directly - Modify the clojure.tools.cli code to replace calls to Java with calls to C# - Add the source code for clojure.math.numeric-tower - Replace a few Java calls in my code with calls to C#

--- src/nash_clojure/core.clj
+++ src/nash_clojure/core.clj
@@ -1,9 +1,175 @@
 (ns
   #^{:author "Josh Peterson"
      :doc "Functions used to categorize Nash solutions to two-player games"} 
-  nash-clojure.core (:gen-class))
-(use 'clojure.math.numeric-tower)
-(use '[clojure.tools.cli :only [cli]])
+  core (:gen-class)
+
+;
+; This is the source code for clojure.tools.cli. It is included here so that
+; the calls to Java interop can be changed to call to the corresponding
+; C# methods.
+;
+(:use [clojure.string :only (replace)]
+      [clojure.pprint :only (pprint cl-format)])
+(:refer-clojure :exclude [replace]))
+
+(defn- build-doc [{:keys [switches docs default]}]
+  [(apply str (interpose ", " switches))
+   (or (str default) "")
+   (or docs "")])
+
+(defn- banner-for [specs]
+  (println "Usage:")
+  (println)
+  (let [docs (into (map build-doc specs)
+                   [["--------" "-------" "----"]
+                    ["Switches" "Default" "Desc"]])
+        max-cols (->> (for [d docs] (map count d))
+                      (apply map (fn [& c] (apply vector c)))
+                      (map #(apply max %)))
+        vs (for [d docs]
+             (mapcat (fn [& x] (apply vector x)) max-cols d))]
+    (doseq [v vs]
+      (cl-format true "~{ ~vA  ~vA  ~vA ~}" v)
+      (prn))))
+
+(defn- name-for [k]
+  (replace k #"^--no-|^--\[no-\]|^--|^-" ""))
+
+(defn- flag-for [^String v]
+  (not (.StartsWith v "--no-")))
+
+(defn- opt? [^String x]
+  (.StartsWith x "-"))
+
+(defn- flag? [^String x]
+  (.StartsWith x "--[no-]"))
+
+(defn- end-of-args? [x]
+  (= "--" x))
+
+(defn- spec-for
+  [arg specs]
+  (->> specs
+       (filter (fn [s]
+                   (let [switches (set (s :switches))]
+                     (contains? switches arg))))
+       first))
+
+(defn- default-values-for
+  [specs]
+  (reduce (fn [m s]
+            (if (contains? s :default)
+              (assoc m (:name s) (:default s))
+              m))
+          {} specs))
+
+(defn- apply-specs
+  [specs args]
+  (loop [options    (default-values-for specs)
+         extra-args []
+         args       args]
+    (if-not (seq args)
+      [options extra-args]
+      (let [opt  (first args)
+            spec (spec-for opt specs)]
+        (cond
+         (end-of-args? opt)
+         (recur options (into extra-args (vec (rest args))) nil)
+
+         (and (opt? opt) (nil? spec))
+         (throw (Exception. (str "'" opt "' is not a valid argument")))
+         
+         (and (opt? opt) (spec :flag))
+         (recur (assoc options (spec :name) (flag-for opt))
+                extra-args
+                (rest args))
+
+         (opt? opt)
+         (recur (assoc options (spec :name) ((spec :parse-fn) (second args)))
+                extra-args
+                (drop 2 args))
+
+         :default
+         (recur options (conj extra-args (first args)) (rest args)))))))
+
+(defn- switches-for
+  [switches flag]
+  (-> (for [^String s switches]
+        (cond
+         (and flag (flag? s))            [(replace s #"\[no-\]" "no-") (replace s #"\[no-\]" "")]
+         (and flag (.StartsWith s "--")) [(replace s #"--" "--no-") s]
+         :default                        [s]))
+      flatten))
+
+(defn- generate-spec
+  [raw-spec]
+  (let [[switches raw-spec] (split-with #(and (string? %) (opt? %)) raw-spec)
+        [docs raw-spec]     (split-with string? raw-spec)
+        options             (apply hash-map raw-spec)
+        aliases             (map name-for switches)
+        flag                (or (flag? (last switches)) (options :flag))]
+    (merge {:switches (switches-for switches flag)
+            :docs     (first docs)
+            :aliases  (set aliases)
+            :name     (keyword (last aliases))
+            :parse-fn identity
+            :flag     flag}
+           (when flag {:default false})
+           options)))
+
+(defn cli
+  "Parse the provided args using the given specs. Specs are vectors
+  describing a command line argument. For example:
+
+  [\"-p\" \"--port\" \"Port to listen on\" :default 3000 :parse-fn #(Integer/parseInt %)]
+
+  First provide the switches (from least to most specific), then a doc
+  string, and pairs of options.
+
+  Valid options are :default, :parse-fn, and :flag. See
+  https://github.com/clojure/tools.cli/blob/master/README.md for more
+  detailed examples.
+
+  Returns a vector containing a map of the parsed arguments, a vector
+  of extra arguments that did not match known switches, and a
+  documentation banner to provide usage instructions."
+  [args & specs]
+  (let [specs (map generate-spec specs)]
+    (let [[options extra-args] (apply-specs specs args)
+          banner  (with-out-str (banner-for specs))]
+      [options extra-args banner])))
+
+;
+; This is the source code for clojure.math.numeric-tower that is used by the normal
+; nash-clojure code. It is included here because I was unable to get the Clojure CLR
+; comiler to load the numeric_tower.clj file correctly.
+;
+
+(def ^{:private true} minus (first [-' -]))
+(def ^{:private true} mult (first [*' *]))
+(def ^{:private true} plus (first [+' +]))
+(def ^{:private true} dec* (first [dec' dec]))
+(def ^{:private true} inc* (first [inc' inc]))
+
+(defn- expt-int [base pow]
+  (loop [n pow, y (num 1), z base]
+    (let [t (even? n), n (quot n 2)]
+      (cond
+       t (recur n y (mult z z))
+       (zero? n) (mult z y)
+       :else (recur n (mult z y) (mult z z))))))
+
+(defn expt
+  "(expt base pow) is base to the pow power.
+Returns an exact number if the base is an exact number and the power is an integer, otherwise returns a double."
+  [base pow]
+  (if (and (not (float? base)) (integer? pow))
+    (cond
+     (pos? pow) (expt-int base pow)
+     (zero? pow) 1
+     :else (/ 1 (expt-int base (minus pow))))
+    (Math/pow base pow)))
+
 
 ;
 ; Two player game strategy generation
@@ -123,12 +289,11 @@
   (let [[options args banner]
     (cli args
       ["-h" "--help" "Show help" :flag true]
-      ["-r" "--rows" "Number of rows in the game" :parse-fn #(Integer. %)] 
-      ["-c" "--columns" "Number of columns in the game" :parse-fn #(Integer. %)]
-      ["-t" "--threads" "Number of threads to use" :parse-fn #(Integer. %)])]
+      ["-r" "--rows" "Number of rows in the game" :parse-fn #(Int32/Parse %)] 
+      ["-c" "--columns" "Number of columns in the game" :parse-fn #(Int32/Parse %)]
+      ["-t" "--threads" "Number of threads to use" :parse-fn #(Int32/Parse %)])]
   (when (:help options)
       (println banner)
-      (System/exit 0))
+      (Environment/Exit 0))
     
-    (println (time (pcategorize-nash-games (:rows options) (:columns options) (:threads options)))))
-  (System/exit 0))
+    (println (time (pcategorize-nash-games (:rows options) (:columns options) (:threads options))))))
